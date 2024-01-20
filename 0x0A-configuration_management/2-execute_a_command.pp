075da311cf6b1984b509542786781b9489fb5776
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-20 02:31:47 +0100

    Delete 0x0A-configuration_management/2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Puppet Manifest for Killing a Process named "killmenow"
-# Ensure all files end with a new line
-exec { 'kill':
-  command => 'pkill -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin']
-}
