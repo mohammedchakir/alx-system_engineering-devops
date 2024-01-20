e6728680a2e40962b500b68a68290b3bd62d8a42
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-19 17:19:33 -0800

    add

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Puppet Manifest for Killing a Process named "killmenow"
 # Ensure all files end with a new line
-exec { 'killmenow':
-  command => '/usr/bin/pkill -f killmenow',
-  onlyif  => '/usr/bin/pgrep -f killmenow',
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
