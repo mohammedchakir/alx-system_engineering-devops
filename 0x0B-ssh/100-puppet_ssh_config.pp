93240b9888c4e5cae2b07a74210809d463fbbc7a
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-20 05:54:22 -0800

    addong new file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Script in Puppet to generate an SSH configuration file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
