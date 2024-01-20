a959dc8c871469ca3388db039e7c664248d9bdf0
mohammedchakir <zerroukimedchakir@gmail.com>
2024-01-20 02:15:50 -0800

    add

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,9 @@
 # Puppet Manifest for Installing Flask using pip3
+package { 'werkzeug':
+  ensure   => '0.16.1',
+  provider => 'pip',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
