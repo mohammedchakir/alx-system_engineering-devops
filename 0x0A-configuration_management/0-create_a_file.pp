58cb40dbb86a329593d3a4c31093dd2b7d6a69d8
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-01-20 02:31:26 +0100

    Delete 0x0A-configuration_management/0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Puppet Manifest for Creating a File in /tmp
-# Ensure all files end with a new line
-file { '/tmp/school':
-ensure  => file,
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',
-}
