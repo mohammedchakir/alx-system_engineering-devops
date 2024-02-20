3ea3a84b770dbc6a78749a1eca6146aa61841ee7
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-20 03:22:34 +0100

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     response = requests.get(url)
     users = response.json()
 
-    dictionary = {}
+    dict = {}
     for user in users:
         user_id = user.get('id')
         username = user.get('username')
@@ -22,12 +22,11 @@ if __name__ == '__main__':
         url = url + '/todos/'
         response = requests.get(url)
         tasks = response.json()
-        dictionary[user_id] = []
+        dict[user_id] = []
         for task in tasks:
-            dictionary[user_id].append({
+            dict[user_id].append({
                 "task": task.get('title'),
                 "completed": task.get('completed'),
-                "username": username
-            })
+                "username": username})
     with open('todo_all_employees.json', 'w') as file:
-        json.dump(dictionary, file)
+        json.dump(dict, file)
