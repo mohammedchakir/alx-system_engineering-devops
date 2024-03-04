b4c29b774a0a01b50bddad1f5a6b4aa2963e9e4e
mohammedchakir <zerroukimedchakir@gmail.com>
2024-03-04 03:51:11 +0100

    first commit of the project

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and returns the number of subscribers"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+    Args:
+        subreddit (str): The name of the subreddit.
+    Returns:
+        int: The number of subscribers for the subreddit.
+        Returns 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyBot/0.1'}  # Setting a custom User-Agent
+    response = get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
