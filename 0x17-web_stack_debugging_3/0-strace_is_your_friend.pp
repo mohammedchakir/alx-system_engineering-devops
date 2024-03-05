0232762f3f7e180e010d27298cea7f69bd980b16
mohammedchakir <zerroukimedchakir@gmail.com>
2024-03-05 23:32:50 +0000

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 # Tmux find that the error is a "phpp" located in /var/www/html/wp-settings.php
 # Uses of exec to correct line in wp-settings.php using sed
 exec { 'fix_wp_settings_error':
-  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path        => '/usr/local/bin:/bin/',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin:/bin/',
 }
