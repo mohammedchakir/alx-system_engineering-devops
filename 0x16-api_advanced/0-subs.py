8a494b5e88ebe77124064f57a1c3802e5b198f7f
mohammedchakir <zerroukimedchakir@gmail.com>
2024-03-05 12:44:12 +0100

    searching solutions

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,12 +14,17 @@ def number_of_subscribers(subreddit):
         int: The number of subscribers for the subreddit.
         Returns 0 if the subreddit is invalid.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyBot/0.1'}
-    response = get(url, headers=headers, allow_redirects=False)
+    try:
+        user_agent = {'User-Agent': 'MyBot/1.0'}
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        response = get(url, headers=user_agent)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            print(f"Error: Unable to access subreddit '{subreddit}'")
+            return 0
+    except Exception as e:
+        print(f"Error: {e}")
         return 0
